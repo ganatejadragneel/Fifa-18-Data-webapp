ea0350706f4c89732e118edeb8f7a1b90deee4c2
Ganateja Akula <ganateja.akula1998@mail.com>
2018-06-15 18:05:56 +0530

    Initial

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+    
+	constructor(props){
+		super(props)
+		this.state={
+			
+		}
+	}
+	
+	render() {
+	  
+    return (
+	<div>
+	<div id="first">
+
+	</div>
+      <div className="main">
+		<h1><center>To Harshita</center></h1>
+		<h3><center>You are too Cute</center></h3>
+      </div>
+	  </div>
+    );
+  }
+}
+
+export default App;
