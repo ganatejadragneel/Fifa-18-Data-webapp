65329fa5486e5d5d595f61f53077a3a5be13c3b9
Ganateja Akula <ganateja.akula1998@mail.com>
2018-06-18 00:19:08 +0530

    basic done

--- src/components/keyplayers.js
+++ src/components/keyplayers.js
@@ -1,14 +1,130 @@
 import React from 'react'
-import { Badge } from 'reactstrap';
+import { Badge,Card, CardImg, CardText, CardBody,CardTitle, CardSubtitle } from 'reactstrap';
 import './keyplayers.css'
 
 export class KeyPlayers extends React.Component{
 	render(){
 		return(
 			<div>
-				<div className="outerbox">
-					<h2 className="badge"><Badge color="success">#1 - Lionel Messi (Argentina)</Badge></h2>
-				</div>
+					<div id="badger"><h2><Badge color="success">#1 - Lionel Messi (Argentina)</Badge></h2></div>
+					<div>
+					<Card>
+						<CardImg src="https://statics.sportskeeda.com/editor/2017/10/8164d-1507294577-800.jpg" alt="Messi" />
+						<CardBody>
+							<CardTitle>Lionel Messi</CardTitle>
+							<CardSubtitle>Argentina</CardSubtitle>
+							<CardText>Lionel Messi has been terrorizing European defences for over a decade now, and has proved himself with FC Barcelona times and times again. His trophy cabinet speaks for itself. However, what the Barça star is still missing, is a major achievement with Argentina's first team.Everybody knows that when the Tango star is on fire, it would be extremely complicated for any defence to stop. Messi fans will be hoping that he inspires Argentina to a victory against Ecuador on Tuesday, before starring for them in June and July.</CardText>
+						</CardBody>
+					</Card>
+					</div>
+					
+					<div id="badger"><h2><Badge color="success">#2 - Neymar (Brazil)</Badge></h2></div>
+					<div>
+					<Card>
+						<CardImg src="https://statics.sportskeeda.com/editor/2017/10/626a2-1507294181-800.jpg" alt="Messi" />
+						<CardBody>
+							<CardTitle>Neymar</CardTitle>
+							<CardSubtitle>Brazil</CardSubtitle>
+							<CardText>Neymar shocked the whole world when he completed his move from FC Barcelona to Paris Saint Germain in the summer, but the Brazilian is proving every week, why this controversial decision could be exactly what he needed in order to reach the next level of stardom.Following the appointment of Tite, Brazil have cemented themselves as the stronger side in South America, and are leading the CONMEBOL qualifiers with a gap ten-point gap over Uruguay, at second place. And Neymar is, without any doubt, the star of this eye-catching Brazil side.</CardText>
+						</CardBody>
+					</Card>
+					</div>
+					
+					<div id="badger"><h2><Badge color="success">#3 - Toni Kroos (Germany)</Badge></h2></div>
+					<div>
+					<Card>
+						<CardImg src="https://statics.sportskeeda.com/editor/2017/10/e2458-1507293444-800.jpg" alt="Messi" />
+						<CardBody>
+							<CardTitle>Toni Kroos</CardTitle>
+							<CardSubtitle>Germany</CardSubtitle>
+							<CardText>Toni Kroos has won it all, at both club level and international football. It is crazy to think that despite all his achievements, he is still only 27-years old and is heading for the next World Cup as Germanys midfield architect.Germany are, as always, one of the biggest favorites to emerge victorious in any tournament they take part in. The reigning world champions are motivated more than ever, because a triumph in Russia would mean matching Brazil's record as the only country to have won the competition on five different occasions.</CardText>
+						</CardBody>
+					</Card>
+					</div>
+					
+					<div id="badger"><h2><Badge color="success">#4 - Isco (Spain)</Badge></h2></div>
+					<div>
+					<Card>
+						<CardImg src="https://statics.sportskeeda.com/editor/2017/10/32735-1507293847-800.jpg" alt="Messi" />
+						<CardBody>
+							<CardTitle> Isco </CardTitle>
+							<CardSubtitle>Spain</CardSubtitle>
+							<CardText>Spain's national team have come a very long way since their last three disappointing tournaments namely - the 2013 Confederations Cup when they lost 3-0 to Brazil, the 2014 World Cup when Netherlands smashed them 5-1, and the Euros in 2016 when they were eliminated by Italy.La Furia Roja is one of the favorites to conquer the next World Cup in Moscow, as they have a very balanced side, with arguably, the best midfield controllers in the world. But one man who stands out in specific, is Francisco Román Alarcón Suárez.</CardText>
+						</CardBody>
+					</Card>
+					</div>
+					
+					<div id="badger"><h2><Badge color="success">#5 - Cristiano Ronaldo (Portugal)</Badge></h2></div>
+					<div>
+					<Card>
+						<CardImg src="https://statics.sportskeeda.com/editor/2017/10/efdbf-1507754785-800.jpg" alt="Messi" />
+						<CardBody>
+							<CardTitle>Cristiano Ronaldo</CardTitle>
+							<CardSubtitle>Portugal</CardSubtitle>
+							<CardText>Cristiano Ronaldo has been one of the most dominant figures in world football in the past decade, and has won all possible cups at club level.But, in the summer of 2016, the Portuguese was able to guide his nation to the first major title in its history, upon claiming the Euro Cup. An achievement which no doubt, cemented Cristiano legacy as one of the greatest footballers in the history of the game.After helping Portugal top their qualifying group, ahead of Switzerland, on goal difference, Ronaldo will be aiming higher this time around, in Russia. The 4-time Ballon d'Or winner, who is regarded by many as the best player in the world, alongside Argentina's Lionel Messi, is expected to captain Portugal in his 4th World Cup participation as a player.</CardText>
+						</CardBody>
+					</Card>
+					</div>
+					
+					<div id="badger"><h2><Badge color="success">#6 - Luis Suárez (Uruguay)</Badge></h2></div>
+					<div>
+					<Card>
+						<CardImg src="https://statics.sportskeeda.com/editor/2017/10/5cc96-1507292497-800.jpg" alt="Messi" />
+						<CardBody>
+							<CardTitle>Luis Suárez</CardTitle>
+							<CardSubtitle>Uruguay</CardSubtitle>
+							<CardText>Uruguay have managed to retrieve a bit of their glorious past, in the last couple of years, as they were able to reach the semi-finals of the 2010 World Cup in South Africa, before eventually claiming the Copa América title on Argentinian soil, in 2011.A lot of La Celeste's success was without any doubt, thanks to the brilliance of the one and only Luis Suárez. The 30-year old has scored 47 goals in 94 appearances, and was decisive enough on the big occasions, more often than not.Suárez is arguably the best striker in the world in the past few years, and has shown his worth in the English Premier League with Liverpool, and then in La Liga with FC Barcelona, where the Uruguayan has basically won it all.</CardText>
+						</CardBody>
+					</Card>
+					</div>
+					
+					<div id="badger"><h2><Badge color="success">#7 - Gabriel Jesus (Brazil)</Badge></h2></div>
+					<div>
+					<Card>
+						<CardImg src="https://statics.sportskeeda.com/editor/2017/10/ef278-1507074010-800.jpg" alt="Messi" />
+						<CardBody>
+							<CardTitle>Gabriel Jesus</CardTitle>
+							<CardSubtitle>Brazil</CardSubtitle>
+							<CardText>The question is yet to be answered as to whether Gabriel Jesus is the best rising talent in the world. While he is very impressive at club level, he may not be "the" most impressive one, among a galaxy of other stars.But, when it comes to international football, it is hard to think of any other youngster who is as crucial for his country-side, as Gabriel Jesus is. The 20-year old has already scored 7 goals and provided 3 assists in just 10 games for the Seleção, all in official World Cup qualifiers.The influence that Gabriel has had is just immense, and he is now considered a permanent entity in Tite's lineup. As a matter of fact, Brazil's national team is being touted as one of the top three favourites, if not the number one favourite, to lift the World Cup trophy in Russia.</CardText>
+						</CardBody>
+					</Card>
+					</div>
+					
+					<div id="badger"><h2><Badge color="success">#8 - Kylian Mbappé (France)</Badge></h2></div>
+					<div>
+					<Card>
+						<CardImg src="https://statics.sportskeeda.com/editor/2017/10/6bdcf-1507073868-800.jpg" alt="Messi" />
+						<CardBody>
+							<CardTitle>Kylian Mbappé</CardTitle>
+							<CardSubtitle>France</CardSubtitle>
+							<CardText>Kylian Mbappé has conquered the hearts of all the lovers of French football. He is considered to be the biggest rising sensation in Ligue 1 at the moment, And how can he not be, with all these bags of potential within him.To put things into perspective, the 18-year old scored 6 goals in the UEFA Champions League last season, to guide AS Monaco to the semi-finals of the edition, for the first time since 2004.With his heroic performances and a full debut for France's national team, it was no surprise that French giants Paris Saint-Germain were quick to acquire his services, for what could add up to a transfer fee of around €180 million, making him the world's second most expensive player in football history, behind his new club-mate, Neymar JR.</CardText>
+						</CardBody>
+					</Card>
+					</div>
+					
+					<div id="badger"><h2><Badge color="success">#9 - Paulo Dybala (Argentina)</Badge></h2></div>
+					<div>
+					<Card>
+						<CardImg src="https://statics.sportskeeda.com/editor/2017/10/f60d5-1507073747-800.jpg" alt="Messi" />
+						<CardBody>
+							<CardTitle>Paulo Dybala</CardTitle>
+							<CardSubtitle>Argentina</CardSubtitle>
+							<CardText>Paolo Dybala is so ridiculously good, that it is so easy to forget the fact that he is only 23-years old and still has a lot of room for development. The Argentinian has already won the hearts of Juventus fanatics, as well as that of all admirers of Italian Calcio.In 7 league games this season, Dybala has already found the net on 10 different occasions. These stats are no doubt, Messi-esque ones. And it is precisely Lionel Messi, to whom the genius of the young rising Argentine, is being compared to.With his movement, dribbling, passing and shooting technique, Paolo is reminiscent of his idol and is being compared to him week in, week out.</CardText>
+						</CardBody>
+					</Card>
+					</div>
+					
+					<div id="badger"><h2><Badge color="success">#10 - Timo Werner (Germany)</Badge></h2></div>
+					<div>
+					<Card>
+						<CardImg src="https://statics.sportskeeda.com/editor/2017/10/6ca36-1507073503-800.jpg" alt="Messi" />
+						<CardBody>
+							<CardTitle>Timo Werner</CardTitle>
+							<CardSubtitle>Germany</CardSubtitle>
+							<CardText>Timo Werner might only be 21-years old, but he has already proved his worth by guiding Germany national team to Confederations Cup triumph in June. Perhaps, a sign of what could happen next year, as the Germans make their comeback to Russian capital Moscow, for the World Cup this time.Werner will be looking to challenge for another Golden Boot award, and who can blame him for being ambitious, when he possesses such fascinating finishing skills and when he is the focal point of Germany's attack.</CardText>
+						</CardBody>
+					</Card>
+					</div>
 			</div>
 	);
 	}
